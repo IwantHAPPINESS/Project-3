97deab2e680dbf2dee76ba2134a1f59afb3136c6
IwantHAPPINESS <magomadovimran142@gmail.com>
2023-08-19 20:57:06 +0300

    Добавлен overlay при активации бургер меня

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
 /*Активация бургер-кнопки и сайд-бара, эфект для кнопки*/
-let windowWidth = window.innerWidth;
-console.log(windowWidth)
 let burgerMenu = document.querySelector('.burger__menu');
 
 burgerMenu.addEventListener('click',burgerActive);
 
 function burgerActive(argument) {
 	let sideBar = document.querySelector('.side__panel')
-
+	let ovelay = document.getElementById('overlay')
 	sideBar.classList.toggle('side__panel-active')
 
 	burgerMenu.classList.toggle('burger__menu-active');
@@ -16,5 +14,5 @@ function burgerActive(argument) {
 	burgerMenu.children[1].classList.toggle('burger__stick2-active');
 	burgerMenu.children[2].classList.toggle('burger__stick3-active');
 
-
+	ovelay.classList.toggle('overlay-active')
 }
